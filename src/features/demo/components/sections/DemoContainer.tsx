34243e2798cc72b0f2a4c11bdbba3f47017285ea
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-08-04 15:24:14 +0700

    feat: add resizable content for demo console card

--- src/features/demo/components/sections/DemoContainer.tsx
+++ src/features/demo/components/sections/DemoContainer.tsx
@@ -1,5 +1,31 @@
+import {
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/shared/components/ui/resizable";
+import DemoCard from "../misc/DemoCard";
+
 const DemoContainer = () => {
-  return <section className="">DemoContainer</section>;
+  return (
+    <section className="w-full h-[calc(100vh-120px)] relative mt-30 p-7 pt-0">
+      <ResizablePanelGroup orientation="horizontal" className="gap-4 h-full">
+        <ResizablePanel defaultSize={"24%"} minSize={300}>
+          <DemoCard>
+            <p>Scenario Web Chat</p>
+          </DemoCard>
+        </ResizablePanel>
+        <ResizablePanel defaultSize={"38%"} minSize={300}>
+          <DemoCard>
+            <p>Action Plan State</p>
+          </DemoCard>
+        </ResizablePanel>
+        <ResizablePanel defaultSize={"38%"} minSize={300}>
+          <DemoCard>
+            <p>Report</p>
+          </DemoCard>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </section>
+  );
 };
 
 export default DemoContainer;
